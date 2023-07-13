6524d7a744b05bb4f5073cc91f44f3c36f37c874
Luiz Gustavo Caciatori <luizgustavo.caciatori@gmail.com>
2023-07-13 16:08:28 -0300

    Add more tests for Drivers.create_driver/1

--- test/formula_one_data_manager/drivers_test.exs
+++ test/formula_one_data_manager/drivers_test.exs
@@ -31,5 +31,44 @@ defmodule FormulaOneDataManager.DriversTest do
       assert_cant_be_blank(changeset, :fastest_laps)
       assert_cant_be_blank(changeset, :race_wins)
     end
+
+    test "returns errors when name has more than 60 characters" do
+      invalid_name = String.duplicate("abc", 21)
+
+      attrs = params_for(:driver, %{name: invalid_name})
+
+      assert {:error, %Ecto.Changeset{} = changeset} = Drivers.create_driver(attrs)
+      assert %{name: ["should be at most 60 character(s)"]} = errors_on(changeset)
+    end
+
+    test "returns errors when name has less than 3 characters" do
+      attrs = params_for(:driver, %{name: "ab"})
+
+      assert {:error, %Ecto.Changeset{} = changeset} = Drivers.create_driver(attrs)
+      assert %{name: ["should be at least 3 character(s)"]} = errors_on(changeset)
+    end
+
+    test "returns errors when country has more than 60 characters" do
+      invalid_name = String.duplicate("abc", 21)
+
+      attrs = params_for(:driver, %{country: invalid_name})
+
+      assert {:error, %Ecto.Changeset{} = changeset} = Drivers.create_driver(attrs)
+      assert %{country: ["should be at most 60 character(s)"]} = errors_on(changeset)
+    end
+
+    test "returns errors when country has less than 3 characters" do
+      attrs = params_for(:driver, %{country: "ab"})
+
+      assert {:error, %Ecto.Changeset{} = changeset} = Drivers.create_driver(attrs)
+      assert %{country: ["should be at least 3 character(s)"]} = errors_on(changeset)
+    end
+
+    test "returns errors when driver is not over 18 years old" do
+      attrs = params_for(:driver, %{date_of_birth: Faker.Date.date_of_birth(1..17)})
+
+      assert {:error, %Ecto.Changeset{} = changeset} = Drivers.create_driver(attrs)
+      assert %{date_of_birth: ["driver should be over 18 years old"]} = errors_on(changeset)
+    end
   end
 end
