bf483b350dd923a4ad480493a4745252893c42b3
Luiz Gustavo Caciatori <luizgustavo.caciatori@gmail.com>
2023-07-17 19:01:44 -0300

    Add default value for integer values on drivers

--- priv/repo/migrations/20230714141248_make_all_driver_fields_required.exs
+++ priv/repo/migrations/20230714141248_make_all_driver_fields_required.exs
@@ -3,9 +3,9 @@ defmodule FormulaOneDataManager.Repo.Migrations.MakeAllDriverFieldsRequired do
 
   def change do
     alter table(:drivers) do
-      modify :poles, :integer, null: false, from: {:integer, null: true}
-      modify :race_wins, :integer, null: false, from: {:integer, null: true}
-      modify :fastest_laps, :integer, null: false, from: {:integer, null: true}
+      modify :poles, :integer, null: false, default: 0, from: {:integer, null: true}
+      modify :race_wins, :integer, null: false, default: 0, from: {:integer, null: true}
+      modify :fastest_laps, :integer, null: false, default: 0, from: {:integer, null: true}
     end
   end
 end
