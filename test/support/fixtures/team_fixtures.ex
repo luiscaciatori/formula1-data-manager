e7e04722ecb735001d900344e27b5adb564ff4a5
Luiz Gustavo Caciatori <luizgustavo.caciatori@gmail.com>
2023-07-19 15:40:49 -0300

    Create Teams fixtures

---
+++ test/support/fixtures/team_fixtures.ex
@@ -0,0 +1,44 @@
+defmodule FormulaOneDataManager.TeamFixtures do
+  alias Faker.Person
+
+  @names ~w(
+    Oracle Red Bull Racing
+    Mercedes-AMG Petronas F1 Team
+    Aston Martin Aramco Cognizant F1 Team
+    Scuderia Ferrari
+    Mclaren F1 Team
+    BWT Alpine F1 Team
+    Willians Racing
+    Money Graan Haas F1 Team
+    Alfa Romeo F1 Team Stake
+    Scuderia AlphaTauri
+  )
+
+  def params_for(:team, attrs \\ %{}) do
+    blueprint = %{
+      name: Enum.random(@names),
+      principal: Person.name(),
+      poles: random_number(),
+      race_wins: random_number(),
+      fastest_lap: random_number(),
+      championship_wins: random_number()
+    }
+
+    Map.merge(blueprint, attrs)
+  end
+
+  def invalid_params_for(:team, attrs \\ %{}) do
+    blueprint = %{
+      principal: nil,
+      name: nil,
+      poles: nil,
+      race_wins: nil,
+      fastest_lap: nil,
+      championship_wins: nil,
+    }
+
+    Map.merge(blueprint, attrs)
+  end
+
+  defp random_number, do: Enum.random(15..150)
+end
